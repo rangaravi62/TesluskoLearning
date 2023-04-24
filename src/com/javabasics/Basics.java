2d92725a9328410b8c7c2b45307ef22b02bd702f
rangaravi10 <raviteja.kr@customerxps.com>
2023-04-25 03:13:42 +0530

    commited java basics files

---
+++ src/com/javabasics/Basics.java
@@ -0,0 +1,32 @@
+package com.javabasics;
+
+public class Basics {
+    public static void main(String[] args) {
+//        int a = 1;
+//        byte b = 127;
+//        float c = 1.9f;
+//        double d = 5.5;
+//        long e = 12345678L;
+//        boolean f = true;
+//        char g = 'r';
+//        short h = 2;
+
+//      --implicit type casting--
+//        byte a =  10;
+//        double b = a ;
+
+
+//      --explicit type casting--
+//        double a = 10;
+//        byte b = (byte) a;
+
+
+
+
+
+
+
+
+
+    }
+}
